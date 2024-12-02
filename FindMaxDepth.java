2be742c9db7430b206c9d3f8002a021064d8f458
g7 <99476075+Gayatri-7@users.noreply.github.com>
2024-12-02 16:06:11 +0530

    Create FindMaxDepth.java

---
+++ FindMaxDepth.java
@@ -0,0 +1,28 @@
+//code for finding max depth of a tree node
+class Treenode{
+  int val;
+  Treenode right;
+  Treenode left;
+  Treenode(int val){this.val=val;}
+}
+public class FindMaxDepth{
+  public int maxdepth(Treenode root){
+  if(root==null){return 0;}
+    else{
+      return Math.max(maxdepth(root.right),maxdepth(root.left))+1;
+      
+    }
+  }
+  public static void main(String[] args){
+   
+    Treenode root=new Treenode(1);
+    root.right=new Treenode(2);
+    root.left=new Treenode(3);
+    root.right.right=new Treenode(4);
+    root.right.left=new Treenode(5);
+
+    FindMaxDepth f=new FindMaxDepth();
+   System.out.println(f.maxdepth(root));
+    
+  }
+}
